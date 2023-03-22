306a1175360737e8122e7660e0942b4080428190
seonghun <tjdgns8439@naver.com>
2023-03-22 21:59:11 +0900

    Update PostsService.java

--- src/main/java/hello/springboot/service/posts/PostsService.java
+++ src/main/java/hello/springboot/service/posts/PostsService.java
@@ -22,9 +22,11 @@ public class PostsService {
     }
 
     @Transactional
-    public Long update(Long id, PostsUpdateRequestDto postsUpdateRequestDto){
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
         Posts posts = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id =" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
         return id;
     }
 
