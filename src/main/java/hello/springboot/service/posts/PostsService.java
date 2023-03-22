5b94208e3642697e8618efc8e6b764d466131279
seonghun <tjdgns8439@naver.com>
2023-03-22 20:50:38 +0900

    Create PostsService.java

---
+++ src/main/java/hello/springboot/service/posts/PostsService.java
@@ -0,0 +1,37 @@
+package hello.springboot.service.posts;
+
+
+import hello.springboot.domain.posts.Posts;
+import hello.springboot.domain.posts.PostsRepository;
+import hello.springboot.web.dto.PostsResponseDto;
+import hello.springboot.web.dto.PostsSaveRequestDto;
+import hello.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto postsUpdateRequestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id =" + id));
+        return id;
+    }
+
+    @Transactional
+    public PostsResponseDto findById (Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
+    }
+}
